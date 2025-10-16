22d408612526eed7204189e2bbc49e2d1e346e14
L.SEVAT <ludovic.sevat@gmail.com>
2025-10-16 21:24:13 +0200

    Build config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/chess-trainer/", // ← ex: "/chess-trainer-v2/"
+  base: "/Chess-Trainer/", 
 });
